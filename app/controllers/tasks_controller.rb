28fba3edcc0a14688ad2e9a66e2b5b6c3e0a15d4
Selin Immel <selin.immel@whu.edu>
2019-05-21 14:55:12 +0800

    user can now add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,17 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def find
+  def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(title: params[:task][:title], details: params[:task][:details])
+    @task.save
+    redirect_to tasks_path
   end
 end
