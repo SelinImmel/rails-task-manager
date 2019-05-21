a1d778587120057de997fcbb92fe782832910b1d
Selin Immel <selin.immel@whu.edu>
2019-05-21 15:12:14 +0800

    a user can now edit tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,4 +16,14 @@ class TasksController < ApplicationController
     @task.save
     redirect_to tasks_path
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: params[:task][:title], details: params[:task][:details])
+    redirect_to task_path(@task)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # listing all tasks
   get 'tasks', to: 'tasks#index', as: :tasks
+
+  # adding and saving a new task
   get 'tasks/new', to: 'tasks#new', as: :new
-  get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create', as: :create
+
+  # details page of single task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # editing and updating a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :update
 end
